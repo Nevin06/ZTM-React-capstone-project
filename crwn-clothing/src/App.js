5cd5946b6a293da12ee3cf777631eec56f5df5a6
Thomas <thomasnevink@gmail.com>
2024-03-27 14:52:55 +0530

    Navigation Bar Component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,5 +1,16 @@
 import Home from './routes/home/home.component'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom'
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
 
 const Shop = () => {
   return (
@@ -10,7 +21,8 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={ <Home /> } >
+      <Route path='/' element= { <Navigation /> } >
+        <Route index element={ <Home /> } />
         <Route path='shop' element={ <Shop /> } />
       </Route>
     </Routes>
