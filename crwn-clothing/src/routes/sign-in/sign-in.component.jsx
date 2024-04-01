87c730af072467dad6be037131df465d73cdc4f4
Thomas <thomasnevink@gmail.com>
2024-04-01 09:06:34 +0530

    Sign In With Redirect

--- crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,18 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+import { auth, signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+    useEffect(() => {
+        async function fetchData() {
+            const response = await getRedirectResult(auth);
+            // console.log(response);
+            if (response){
+                const userDocRef = await createUserDocumentFromAuth(response.user);
+            }
+        };
+        fetchData();
+    }, []);
     const logGoogleUser = async () => {
         // const response = await signInWithGooglePopup();
         // console.log(response);
@@ -10,10 +22,16 @@ const SignIn = () => {
         const userDocRef = await createUserDocumentFromAuth(user);
     };
 
+    // const logGoogleRedirectUser = async () => {
+    //     const { user } = await signInWithGoogleRedirect();
+    //     console.log({user});
+    // };
+
     return (
         <div>
             <h1>SignIn page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
         </div>
     );
 };
