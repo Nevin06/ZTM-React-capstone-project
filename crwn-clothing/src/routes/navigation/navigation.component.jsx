1fe76aec5d147450ae7911c4fb825c296d0271ff
Thomas <thomasnevink@gmail.com>
2024-04-07 10:49:12 +0530

    Cart Icon & Dropdown

--- crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -4,6 +4,8 @@ import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
 import './navigation.styles.scss';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
@@ -26,7 +28,11 @@ const Navigation = () => {
                         </Link>
                     )
                 }
+                <Link className='nav-link' to='/cart'>
+                    <CartIcon />
+                </Link>
             </div>
+            <CartDropdown />
         </div>
         <Outlet />
       </Fragment>
