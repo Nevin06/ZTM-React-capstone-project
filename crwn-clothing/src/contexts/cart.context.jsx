60073456ac8fd1d3b59323e35ef35b3df2e6856f
Thomas <thomasnevink@gmail.com>
2024-04-09 04:35:10 +0530

    Add To Cart Pt.1 & Pt.2

--- crwn-clothing/src/contexts/cart.context.jsx
+++ crwn-clothing/src/contexts/cart.context.jsx
@@ -1,13 +1,57 @@
 import { createContext, useState } from "react";
 
+// Helper function to add a product to the cart or increment its quantity
+const addCartItem = (cartItems, productToAdd) => {
+  // find if cartItems contains productToAdd
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === productToAdd.id
+  );
+
+  // if found, increment quantity
+  if (existingCartItem) {
+    // Product already in cart, increment quantity
+    return cartItems.map(cartItem =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  } else {
+    // Product not in cart, add it
+    // return new array with modified cartItems/ new cartItem
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
+  }
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
-  setIsCartOpen: () => {}
+  setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  setCartItems: () => {}
 });
+
+/*
+
+product
+{
+  id, name, price, imageUrl
+}
+
+cartItem
+{
+  id, name, price, imageUrl, quantity
+}
+*/
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = { isCartOpen, setIsCartOpen };
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+      setCartItems(addCartItem(cartItems, productToAdd));
+    };
+
+    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
 
     return (
       <CartContext.Provider value={value}>
