8d367332f8b4393cbccfd242de2e4f9d2f646fca
Thomas <thomasnevink@gmail.com>
2024-04-08 15:30:56 +0530

    Toggle Cart Open

--- crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -6,9 +6,12 @@ import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import { CartContext } from '../../contexts/cart.context';
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
+
     return (
       <Fragment>
         <div className='navigation'>
@@ -28,11 +31,9 @@ const Navigation = () => {
                         </Link>
                     )
                 }
-                <Link className='nav-link' to='/cart'>
-                    <CartIcon />
-                </Link>
+                <CartIcon />
             </div>
-            <CartDropdown />
+            {isCartOpen && <CartDropdown />}
         </div>
         <Outlet />
       </Fragment>
