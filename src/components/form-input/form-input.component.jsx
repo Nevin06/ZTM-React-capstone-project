c054f60a0c47dc2c1f1d01d2cb4c67959d5cc926
Thomas <thomasnevink@gmail.com>
2024-04-17 14:11:45 +0530

    Filter Unknown Props in FormInput component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,12 +1,15 @@
 import { FormInputLabel, Input, Group } from './form-input.styles';
+import { StyleSheetManager } from 'styled-components';
 const FormInput = ({ label, ...otherProps}) => {
     return (
         <Group>
             <Input {...otherProps} />
             {label && (
-                <FormInputLabel shrink={otherProps.value.length}>
-                    {label}
-                </FormInputLabel>
+                <StyleSheetManager shouldForwardProp={(prop) => prop !== 'shrink'}>
+                    <FormInputLabel shrink={otherProps.value.length}>
+                        {label}
+                    </FormInputLabel>
+                </StyleSheetManager>
             )}
          </Group>
     );
